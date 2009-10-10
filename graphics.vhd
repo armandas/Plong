844e95c3b5b3c1c0d2eee8beb5aae6040370f4dc
Armandas Jarusauskas <jarusauskas@gmail.com>
2009-10-10 10:50:40 +0100

    Tidying up plong.vhd and adding graphics dispatcher.

---
+++ graphics.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity graphics is
+end graphics;
+
+architecture dispatcher of graphics is
+begin
+
+end dispatcher;
